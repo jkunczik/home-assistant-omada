92adf207fc8dc512d42b25b911b609bcb4623088
Jeroen Oomkes <jeruntu@gmail.com>
2024-11-11 23:59:49 +0100

    Restore backup old file structure

--- Omada Beta/entrypoint.sh
+++ Omada Beta/entrypoint.sh
@@ -8,7 +8,22 @@
 bashio::log.info "Create 'logs' directory inside persistent /data volume, if it doesn't exist."
 mkdir -p "/data/logs"
 
-[ ! -d /data/data ] && bashio::log.info "/data/data created from backup" && cp -r /opt/tplink/EAPController/data_backup /data/data
+if [ ! -d /data/data ]; then
+  bashio::log.info "/data/data created from docker image backup" && cp -r /opt/tplink/EAPController/data_backup /data/data
+
+  # Check if old directory structure is in place (/data) and copy to (/data/data)
+  # This can be removed in the future when everyone has upgraded
+  directories=(db keystore pdf)
+  for dir in "${directories[@]}"; do
+    if [ -d "/data/$dir" ]; then
+      cp -r /data/$dir "/data/data/"
+      rm -rf /data/$dir
+      bashio::log.info "Migrate from old Add-On file structure. Copied /data/$dir to /data/data/$dir"
+    else
+      bashio::log.info "Already in new file structure. /data/$dir does not exist, skipping."
+    fi
+  done
+fi
 
 # set permissions on /data directory for home assistant persistence
 chown -R 508:508 "/data"
