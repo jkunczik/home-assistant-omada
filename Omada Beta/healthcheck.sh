75617b844bb954fbbbca206483154967b6142b18
Jeroen Oomkes <jeruntu@gmail.com>
2024-11-12 15:35:53 +0100

    Use submodule for upstream

--- Omada Beta/healthcheck.sh
+++ Omada Beta/healthcheck.sh
@@ -1,3 +1,1 @@
-#!/bin/sh
-
-wget --quiet --tries=1 --no-check-certificate -O /dev/null --server-response --timeout=5 "https://127.0.0.1:${MANAGE_HTTPS_PORT:-8043}/login" || exit 1
+mbentley/healthcheck.sh
